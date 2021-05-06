fe265db01adc02a597a9e254df0a866b6d22e315
rmshakthi99 <82799369+rmshakthi99@users.noreply.github.com>
2021-05-06 16:27:58 +0530

    Add files via upload

---
+++ TransportDALLibrary/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportDALLibrary
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Password { get; set; }
+        public string Location { get; set; }
+        public string VehicleNumber { get; set; }
+
+    }
+}
---
+++ TransportDALLibrary/EmployeeDAL.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace TransportDALLibrary
+{
+    public class EmployeeDAL
+    {
+        SqlConnection conn;
+        SqlCommand cmd;
+        string strConnection;
+        public EmployeeDAL()
+        {
+            strConnection = @"server=SHAKTHI-RM\SQLEXPRESS;Integrated security=true;Initial Catalog=dbSoftTransport";
+            conn = new SqlConnection(strConnection);
+
+        }
+        public bool AddEmployee(Employee employee)
+        {
+            cmd = new SqlCommand("proc_InsertEmployee", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            try
+            {
+                cmd.Parameters.AddWithValue("@eName", employee.Name);
+                cmd.Parameters.AddWithValue("@ePassword", employee.Password);
+                cmd.Parameters.AddWithValue("@eLocation", employee.Location);
+                cmd.Parameters.AddWithValue("@ePhone", employee.Phone);
+                if (conn.State == ConnectionState.Open)
+                    conn.Close();
+                conn.Open();
+                int result = cmd.ExecuteNonQuery();
+                if (result > 0)
+                    return true;
+                else
+                    return false;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+        public bool UpdatePassword(Employee employee)
+        {
+            cmd = new SqlCommand("proc_UpdatePassword", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            try
+            {
+                cmd.Parameters.AddWithValue("@eid", employee.id);
+                cmd.Parameters.AddWithValue("@ePassword", employee.Password);
+                if (conn.State == ConnectionState.Open)
+                    conn.Close();
+                conn.Open();
+                int result = cmd.ExecuteNonQuery();
+                if (result > 0)
+                    return true;
+                else
+                    return false;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+
+        }
+        public ICollection<Employee> SelectAllEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+            SqlCommand cmd = new SqlCommand("proc_GetAllEmployees");
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection = conn;
+            SqlDataAdapter daEmployee = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            try
+            {
+                daEmployee.Fill(ds, "Employee");
+                Employee employee;
+                foreach (DataRow dr in ds.Tables[0].Rows)
+                {
+                    employee = new Employee();
+                    employee.id = Convert.ToInt32(dr[0]);
+                    employee.Name = dr[1].ToString();
+                    employee.Password = dr[2].ToString();
+                    employee.Location = dr[3].ToString();
+                    employee.Phone = dr[4].ToString();
+                    employee.VehicleNumber = dr[5].ToString();
+                    employees.Add(employee);
+                }
+                return employees;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            return employees;
+        }
+    }
+}
+
+            
+
+
+                     
+
+
+
+                    
+                
+            
+        
+            
+            
+        
+    
+
